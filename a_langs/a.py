e8868e5fe211575918f6955fa9978ab73f08c6ae
dendanacode <dendanacode@gmail.com>
2020-07-31 13:22:54 -0700

    added anim noun, verbs to A deriver

--- a_langs/a.py
+++ a_langs/a.py
@@ -16,9 +16,15 @@ def a_page():
     show_in = st.checkbox('Show input in output', value=False)
     pos = st.radio('Part of Speech', ['Noun (inan)', 'Noun (anim)', 'Verb', 'Other'], index=3)
     words = []
-    if pos == 'Noun (inan)':
+    if pos == 'Noun (inan)' or pos == 'Noun (anim)':
         definite = st.checkbox('I-class?', value=False)
-        words = st.text_area('Words to derive', value='a,i')
+        if pos == 'Noun (inan)':
+            words = st.text_area('Words to derive (comma-separate singular/plural stems)', value='a,i')
+        elif pos == 'Noun (anim)':
+            words = st.text_area('Words to derive', value='')
+    elif pos == 'Verb':
+        # verb_class = st.radio('Verb Class', ['H', 'V', 'N'], index=0)
+        words = st.text_area('Words to derive (comma-separate pfv/ipfv stems + class [H/V/N])', value='a,i,H')
     else:
         words = st.text_area('Words to derive', value='')
 
@@ -75,6 +81,64 @@ def a_page():
                 input[sg+'/'+pl]['SG'][feature] = sg + suffix
             for feature, suffix in affixes['PL'].items():
                 input[sg+'/'+pl]['PL'][feature] = pl + suffix
+    elif pos == 'Noun (anim)':
+        words2 = words.split('\n')
+        if definite == False:
+            affixes = {'SG':
+                {'NOM':''
+                    , 'NOMPST':'pɨ'
+                    , 'GEN':'nšu'
+                    , 'GENPST':'manšu'
+                    , 'OBL':'n'
+                    , 'OBLPST':'man'
+                }
+                , 'PL': {
+                    'NOM':'m'
+                    , 'NOMPST':'mam'
+                    , 'GEN':'nsVšu'
+                    , 'GENPST':'mansɨšu'
+                    , 'OBL':'nsV'
+                    , 'OBLPST':'mansɨ'
+                }
+            }
+        else:
+            affixes = {'SG':
+                {'NOM':'ʔi'
+                    , 'NOMPST':'pi'
+                    , 'GEN':'nišu'
+                    , 'GENPST':'manišu'
+                    , 'OBL':'ni'
+                    , 'OBLPST':'mani'
+                }
+                , 'PL': {
+                    'NOM':'m'
+                    , 'NOMPST':'mam'
+                    , 'GEN':'nsVšu'
+                    , 'GENPST':'mansɨšu'
+                    , 'OBL':'nsV'
+                    , 'OBLPST':'mansɨ'
+                }
+            }
+        for stem in words2:
+            input[stem] = {'SG':{}, 'PL':{}}
+            for feature, suffix in affixes['SG'].items():
+                input[stem]['SG'][feature] = stem + suffix
+            for feature, suffix in affixes['PL'].items():
+                input[stem]['PL'][feature] = stem + suffix
+
+    elif pos == 'Verb':
+        words2 = [tuple(w.split(',')) for w in words.split('\n')]
+        for stem in words2:
+            stem_key = stem[0] + '/' + stem[1]
+            if stem[2] == 'H':
+                input[stem_key] = {'PFV':stem[0], 'IPFV':stem[1], 'HAB':stem[0] + stem[0]}
+            elif stem[2] == 'V':
+                input[stem_key] = {'PFV':'vA' + stem[0], 'IPFV':'vA' + stem[1], 'HAB':'vA' + stem[0] + stem[0]}
+            elif stem[2] == 'N':
+                input[stem_key] = {'PFV':'ni' + stem[0], 'IPFV':'ni' + stem[1], 'HAB':'ni' + stem[0] + stem[0]}
+
+
+
     else:
         words = words.split('\n')
 
@@ -98,8 +162,8 @@ def a_page():
 
     with open(open_page, 'r') as t:
         ruleset = t.read()
-        output = {'SG':{}, 'PL':{}}
-        if pos == 'Noun (inan)':
+        if pos == 'Noun (inan)' or pos == 'Noun (anim)':
+            output = {'SG':{}, 'PL':{}}
             for form in input:
                 st.write(form)
                 for number in input[form]:
@@ -108,6 +172,15 @@ def a_page():
                 output_df = pd.DataFrame(output)
                 st.write(output_df)
                 output = {'SG':{}, 'PL':{}}
+        elif pos == 'Verb':
+            output = {'PFV':'', 'IPFV':'', 'HAB':''}
+            for form in input:
+                st.write(form)
+                for feature in input[form]:
+                    output[feature] = sce.run([input[form][feature]], ruleset, output='str')
+                output_df = pd.DataFrame(output, index=[0])
+                st.write(output_df)
+                output = {'PFV':'', 'IPFV':'', 'HAB':''}
 
         else:
             output = sce.run(words, ruleset, output='list')
